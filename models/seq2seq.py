0864306d495ff1f1342958e13b90f9e6a304ee96
Germey <cqc@cuiqingcai.com>
2018-06-18 02:33:18 +0800

    update max

--- models/seq2seq.py
+++ models/seq2seq.py
@@ -284,9 +284,10 @@ class Seq2SeqModel():
                 
                 # decoder_masks: [batch_size, reduce_max(decoder_inputs_length)]
                 self.decoder_masks = tf.sequence_mask(lengths=self.decoder_inputs_train_length,
-                                                      maxlen=tf.reduce_max(self.decoder_targets_train_length),
+                                                      maxlen=self.decoder_max_time_steps + 1,
                                                       dtype=self.dtype,
                                                       name='masks')
+                
                 self.logger.debug('decoder_masks %s', self.decoder_masks)
                 
                 # loss
