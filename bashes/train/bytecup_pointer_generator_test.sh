0bae934fabac2ebc978bf839f13a0b99b314e48f
Germey <cqc@cuiqingcai.com>
2018-09-03 15:47:24 +0800

    add test

---
+++ bashes/train/bytecup_pointer_generator_test.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+cd ../..
+python3 train.py\
+    --model_class pointer_generator\
+    --batch_size 5\
+    --hidden_units 200\
+    --embedding_size 200\
+    --attention_units 220\
+    --encoder_depth 1\
+    --decoder_depth 1\
+    --encoder_max_time_steps 1000\
+    --decoder_max_time_steps 20\
+    --display_freq 5\
+    --save_freq 2000\
+    --valid_freq 400\
+    --model_dir checkpoints/bytecup_pointer_generator\
+    --model_name lcsts.ckpt\
+    --source_vocabulary dataset/bytecup/word/vocabs.json\
+    --target_vocabulary dataset/bytecup/word/vocabs.json\
+    --source_train_data dataset/bytecup/word/contents.eval.txt\
+    --target_train_data dataset/bytecup/word/titles.eval.txt\
+    --source_valid_data dataset/bytecup/word/contents.eval.txt\
+    --target_valid_data dataset/bytecup/word/titles.eval.txt\
+    --encoder_vocab_size 30000\
+    --decoder_vocab_size 30000\
+    --cell_type gru\
+    --max_epochs 100000\
+    --extend_vocabs True
